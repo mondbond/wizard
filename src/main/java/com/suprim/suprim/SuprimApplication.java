edc9a842aaa4cdd6c9483d0bffe73ddab09c1f6e
ibahr <vanya0794@gmail.com>
2018-02-23 18:48:43 +0200

    Initial commit

---
+++ src/main/java/com/suprim/suprim/SuprimApplication.java
@@ -0,0 +1,12 @@
+package com.suprim.suprim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuprimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuprimApplication.class, args);
+	}
+}
